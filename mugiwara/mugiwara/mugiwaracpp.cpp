f350637d31bee5d23a0d7fd99a7e55b1e28ba94c
Polina <polinasavan8@gmail.com>
2025-04-09 18:39:57 +0300

    Add files via upload

---
+++ mugiwara/mugiwara/mugiwaracpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main() {
+	const int n = 10;
+	int nums[n];
+	int target[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> nums[i];
+
+	}
+	for (int i = 0; i < n; i++) {
+		int target = nums[i] + nums[i];
+
+	}
+
+}
